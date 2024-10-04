7d576b882902d0617e23fc200660aefe3c4aa042
Jakub Slon <jakub.slon@x-labsystems.co.uk>
2024-10-04 15:39:43 +0100

    move networking

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -1,62 +1,49 @@
-# Create VPC and public subnet
-resource "aws_vpc" "default_vpc" {
-  for_each = toset(local.environments)
+# VPC
 
-  cidr_block           = "10.0.0.0/16" # Adjust if needed per environment
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
-resource "aws_subnet" "public" {
-  for_each = {
-    for env in local.environments :
-    env => range(2)
-  }
 
-  vpc_id                  = aws_vpc.default_vpc[each.key].id
-  cidr_block              = cidrsubnet(aws_vpc.default_vpc[each.key].cidr_block, 8, count.index)
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
 }
 
-# Create aws gw and add a route table
 resource "aws_internet_gateway" "gw" {
-  for_each = toset(local.environments)
-
-  vpc_id = aws_vpc.default_vpc[each.key].id
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route_table" "public" {
-  for_each = toset(local.environments)
-
-  vpc_id = aws_vpc.default_vpc[each.key].id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw[each.key].id
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
 
 resource "aws_route_table_association" "public" {
-  for_each = {
-    for env in local.environments :
-    env => range(2) # Create 2 subnets per environment
-  }
-  subnet_id      = element(aws_subnet.public[each.key].*.id, count.index)
-  route_table_id = aws_route_table.public[each.key].id
+  count          = 2
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
 }
 
 # Security group for ALB
-resource "aws_security_group" "alb_sg" {
-  for_each = toset(local.environments)
 
-  name   = "${each.key}-allow_http"
-  vpc_id = aws_vpc.default_vpc[each.key].id
+resource "aws_security_group" "alb_sg" {
+  name   = "allow_http"
+  vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["86.168.21.174/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -67,38 +54,32 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-# Create ALB and tg
-resource "aws_lb" "default_alb" {
-  for_each = toset(local.environments)
+#ALB 
 
-  name               = module.eg_alb_label[each.key].id
+resource "aws_lb" "main" {
+  name               = module.eg_staging_alb_label.id
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg[each.key].id]
-  subnets            = aws_subnet.public[each.key].*.id
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.public[*].id
 }
 
-resource "aws_lb_target_group" "default_alb_tg" {
-  for_each = toset(local.environments)
-
-  name     = "${module.eg_alb_label[each.key].id}-tg"
+resource "aws_lb_target_group" "main" {
+  name     = "${module.eg_staging_alb_label.id}-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.default_vpc[each.key].id
+  vpc_id   = aws_vpc.main.id
 }
 
-resource "aws_lb_listener" "default_alb_listener" {
-  for_each = toset(local.environments)
-
-  load_balancer_arn = aws_lb.default_alb[each.key].arn
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.default_alb_tg[each.key].arn
+    target_group_arn = aws_lb_target_group.main.arn
   }
 }
-
 # NOTE
 # In order to handle internal HTTPS traffic, listener would need to be using 443 and HTTPS, and will need an SSL policy with certificate ARN.
